317b99884d6e73c22be4a7a61b1e4f5ea5e928d4
shengtang87 <what88887@gmail.com>
2023-05-28 21:57:32 +0800

    modified

--- HW4/Bellman-Ford.c
+++ HW4/Bellman-Ford.c
@@ -11,7 +11,7 @@ struct Edge
     // This structure is equal to an edge. Edge contains two end points. These edges are directed edges so they
 //contain source and destination and some weight. These 3 are elements in this structure
     int source, destination, weight;
-}
+};
  
 // a structure to represent a connected, directed and weighted graph
 struct Graph
@@ -25,14 +25,14 @@ struct Graph
  
 struct Graph* createGraph(int V, int E)
 {
-    struct Graph* graph = (struct Graph*) malloc( sizeof(struct graph);
+    struct Graph* graph = (struct Graph*) malloc( sizeof(struct Graph));
 //Allocating space to structure graph
  
     graph->V = V;   //assigning values to structure elements that taken form user.
  
     graph->E = E;
  
-    graph->>edge = (struct Edge) malloc( graph->E * sizeof( struct Edge ) );
+    graph->edge = (struct Edge*) malloc( graph->E * sizeof( struct Edge ) );
 //Creating "Edge" type structures inside "Graph" structure, the number of edge type structures are equal to number of edges
  
     return ;
